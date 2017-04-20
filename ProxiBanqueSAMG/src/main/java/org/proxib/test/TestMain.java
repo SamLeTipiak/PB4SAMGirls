8c49368f52f183e8f9e540411e463d66321cc292
ghania <ghania.bouzemame@gmail.com>
2017-04-20 19:51:07 +0200

    aurelie : testmain

--- ProxiBanqueSAMG/src/main/java/org/proxib/test/TestMain.java
+++ ProxiBanqueSAMG/src/main/java/org/proxib/test/TestMain.java
@@ -5,8 +5,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.proxib.config.ApplicationConfig;
+import org.proxib.model.Account;
+import org.proxib.model.Account.typeAccount;
 import org.proxib.model.Client;
+import org.proxib.service.IAccountService;
 import org.proxib.service.IClientService;
+import org.proxib.service.ServiceAccount;
 import org.proxib.service.ServiceClient;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -17,23 +21,28 @@ public class TestMain {
 		
 		ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
 		
-		IClientService serviceClient = context.getBean("serviceClient", ServiceClient.class);
-		try {
+//		IClientService serviceClient = context.getBean("serviceClient", IClientService.class);
+//		IAccountService serviceAccount = context.getBean("serviceAccount", ServiceAccount.class);
+//		try {
 
-			Client c1 = new Client("Paul", "Leroy", "17 rue des oliviers 75001 Paris", "paul.leroy@gmail.com" );
-			Client c2 = new Client("Paula", "Lis", "18 rue des oliviers 75001 Paris", "paula.lis@gmail.com");
-			List<Client> clients = new ArrayList<>();
-			clients.add(c1);
-			clients.add(c2);
-			serviceClient.persist(c1);
-			serviceClient.persist(c2);
-			System.out.println(clients);
+//			Client c1 = new Client("Paul", "Leroy", "17 rue des oliviers 75001 Paris", "paul.leroy@gmail.com" );
+//			Client c2 = new Client("Paula", "Lis", "18 rue des oliviers 75001 Paris", "paula.lis@gmail.com");
+//			List<Client> clients = new ArrayList<>();
+//			clients.add(c1);
+//			clients.add(c2);
+//			serviceClient.persist(c1);
+//			serviceClient.persist(c2);
+//			System.out.println(clients);
+//			
+//			Account compte1 = new Account(541, 456.3, typeAccount.CURRENT);
+//			serviceAccount.persist(compte1);
 			
 			
+			
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
 		
 		
 
