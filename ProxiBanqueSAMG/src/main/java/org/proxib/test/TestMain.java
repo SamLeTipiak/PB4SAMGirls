fe90f057485835e5dcd5397da772787edc11a673
ghania <ghania.bouzemame@gmail.com>
2017-04-20 18:18:29 +0200

    ghania testMain listeClients

--- ProxiBanqueSAMG/src/main/java/org/proxib/test/TestMain.java
+++ ProxiBanqueSAMG/src/main/java/org/proxib/test/TestMain.java
@@ -1,6 +1,7 @@
 package org.proxib.test;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.proxib.config.ApplicationConfig;
@@ -18,12 +19,20 @@ public class TestMain {
 		
 		IClientService serviceClient = context.getBean("serviceClient", ServiceClient.class);
 		try {
-			serviceClient.persist(new Client("Paul", "Leroy", "17 rue des oliviers 75001 Paris", "paul.leroy@gmail.com") );
-			serviceClient.persist(new Client("Paula", "Lis", "18 rue des oliviers 75001 Paris", "paula.lis@gmail.com") );
-			List<Client>
+			Client c1 = new Client("Paul", "Leroy", "17 rue des oliviers 75001 Paris", "paul.leroy@gmail.com" );
+			Client c2 = new Client("Paula", "Lis", "18 rue des oliviers 75001 Paris", "paula.lis@gmail.com");
+			List<Client> clients = new ArrayList<>();
+			clients.add(c1);
+			clients.add(c2);
+			serviceClient.persist(c1);
+			serviceClient.persist(c2);
+			System.out.println(clients);
+			
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
 		
 
 	}
