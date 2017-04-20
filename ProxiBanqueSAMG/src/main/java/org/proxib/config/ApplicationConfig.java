9b47c6597c515e3226f654b675aea67f164c31cd
SamLeTipiak <leumas86@hotmail.com>
2017-04-20 16:01:26 +0200

    ghania +ApplicationConfig.java -void.properties

---
+++ ProxiBanqueSAMG/src/main/java/org/proxib/config/ApplicationConfig.java
@@ -0,0 +1,78 @@
+package org.proxib.config;
+
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableJpaRepositories(basePackages = { "org.proxib" })
+@EnableTransactionManagement
+@PropertySource("classpath:application.properties")
+@ComponentScan(basePackages = { "org.proxib" })
+public class ApplicationConfig {
+
+	@Autowired
+	private Environment environment;
+
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(environment.getProperty("db.driverClass"));
+		dataSource.setUrl(environment.getProperty("db.url"));
+		dataSource.setUsername(environment.getProperty("db.username"));
+		dataSource.setPassword(environment.getProperty("db.password"));
+
+		return dataSource;
+
+	}
+
+	@Bean
+	public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+
+		JpaTransactionManager jpaTransactionManager = new JpaTransactionManager();
+		jpaTransactionManager.setEntityManagerFactory(entityManagerFactory);
+
+		return jpaTransactionManager;
+	}
+
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
+		jpaVendorAdapter.setDatabase(Database.MYSQL);
+		jpaVendorAdapter.setShowSql(true);
+
+		return jpaVendorAdapter;
+	}
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
+		entityManagerFactory.setDataSource(dataSource());
+		entityManagerFactory.setJpaVendorAdapter(jpaVendorAdapter());
+		entityManagerFactory.setPackagesToScan("org.proxib");
+
+		Properties jpaProperties = new Properties();
+		jpaProperties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+
+		entityManagerFactory.setJpaProperties(jpaProperties);
+
+		return entityManagerFactory;
+	}
+
+}
