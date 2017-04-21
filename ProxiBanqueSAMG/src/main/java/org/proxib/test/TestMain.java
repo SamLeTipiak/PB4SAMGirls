1c4d1e814747b2b5b0516db85171a70f8d6346d3
ghania <ghania.bouzemame@gmail.com>
2017-04-21 11:36:35 +0200

    space

--- ProxiBanqueSAMG/src/main/java/org/proxib/test/TestMain.java
+++ ProxiBanqueSAMG/src/main/java/org/proxib/test/TestMain.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class TestMain {
 
 	public static void main(String[] args) {
-
+ 
 		ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
 
 		
