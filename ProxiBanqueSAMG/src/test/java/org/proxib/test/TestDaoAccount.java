ae4af823136ff35062248cc008c087f180348098
SamLeTipiak <leumas86@hotmail.com>
2017-04-21 15:15:57 +0200

    test client

--- ProxiBanqueSAMG/src/test/java/org/proxib/test/TestDaoAccount.java
+++ ProxiBanqueSAMG/src/test/java/org/proxib/test/TestDaoAccount.java
@@ -18,66 +18,24 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration(classes = { ApplicationConfig.class })
 public class TestDaoAccount {
 
-	// @Autowired
-	// private IAccountService accountService;
-
-	// IAccountService accountService = new ServiceAccount();
-	// IAccountDao accountDao = new AccountDaoImpl();
 
 	@Autowired
 	IAccountService accountService;
 
-	Account account1 = null;
-	Account account2 = null;
+	Account account1 = new Account(2000, 0.5, typeAccount.CURRENT);
 
-	@Before
-	public void createDB() {
-		account1 = new Account(2000, 0.5, typeAccount.CURRENT);
-		account2 = new Account(2000, 0.5, typeAccount.CURRENT);
-		try {
-			accountService.persist(account1);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
 
-	// @Test
-	// public void testAddAccount() {
-	//
-	//
-	// System.out.println("entrée dans le test addAccount");
-	//
-	//// List<Account> listAccounts = new ArrayList<>();
-	// try {
-	// Account account1 = new Account(2000, 0.5, typeAccount.CURRENT);
-	// accountDao.persist(account1);
-	// System.out.println("persistance okay");
-	// //listAccounts = accountDao.findAll();
-	// //System.out.println("list okay");
-	//// assertEquals(1, accountDao.findAll().size());
-	// assertEquals(typeAccount.CURRENT,
-	// accountDao.findAll().get(0).getTypeAccount());
-	// } catch (Exception e) {
-	// System.out.println("Pb. dans le test");
-	// e.printStackTrace();
-	// }
-	//
-	//// assertEquals(1, listAccounts.size());
-	//
-	//
-	// }
+
 
 	@Test
 	public void testServiceAddAccount() {
 		try {
 			int sizeBefore = accountService.findAll().size();
-			accountService.persist(account2);
+			accountService.persist(account1);
 			assertEquals(sizeBefore + 1, accountService.findAll().size());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
 	}
 
 	@Test
@@ -94,36 +52,28 @@ public class TestDaoAccount {
 	@Test
 	public void testServiceUpdateAccount(){
 		
-		account1.setBalance(300);
 		try {
+			account1=accountService.findAll().get(accountService.findAll().size()-1);
+			account1.setBalance(300);
 			accountService.merge(account1);
 			assertEquals(300, accountService.findById(account1.getId()).getBalance(), 0);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
 	}
+//
+//	@Test
+//	public void testServiceDeleteAccount() {
+//		try {
+//			int sizeBefore = accountService.findAll().size();
+//			accountService.remove(accountService.findAll().get(accountService.findAll().size() - 1).getId());
+//			assertEquals(sizeBefore - 1, accountService.findAll().size());
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//
+//	}
 
-	@Test
-	public void testServiceDeleteAccount() {
-		try {
-			int sizeBefore = accountService.findAll().size();
-			accountService.remove(accountService.findAll().get(accountService.findAll().size() - 1).getId());
-			assertEquals(sizeBefore - 1, accountService.findAll().size());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 
-	}
-
-	@After
-	public void deleteDB() {
-		try {
-			accountService.remove(accountService.findAll().get(accountService.findAll().size()-1).getId());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
 }
