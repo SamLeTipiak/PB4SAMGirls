81861e71062dc440584eb17d7bff02df03d4dd6e
ghania <ghania.bouzemame@gmail.com>
2017-04-21 11:33:16 +0200

    Aurelie : account cotnroler

--- ProxiBanqueSAMG/src/main/java/org/proxib/presentation/AccountController.java
+++ ProxiBanqueSAMG/src/main/java/org/proxib/presentation/AccountController.java
@@ -20,7 +20,7 @@ import org.springframework.stereotype.Component;
 public class AccountController implements Serializable {
 
 	/**
-	 * 
+	 *  
 	 */
 	private static final long serialVersionUID = 1L;
 
