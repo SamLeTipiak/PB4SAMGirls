fa63cddf82cdb1d495b1da1afc9730ea0ff9a003
Ghania <ghania.bouzemame@gmail.com>
2017-04-23 19:31:28 +0200

    ghania liste clients du conseiller 1

--- ProxiBanqueSAMG/src/main/java/org/proxib/test/MainGhania.java
+++ ProxiBanqueSAMG/src/main/java/org/proxib/test/MainGhania.java
@@ -4,38 +4,40 @@ import org.proxib.config.ApplicationConfig;
 import org.proxib.model.Adviser;
 import org.proxib.model.Client;
 import org.proxib.model.CurrentAccount;
-import org.proxib.model.SavingAccount;
-import org.proxib.service.IAccountService;
 import org.proxib.service.IAdviserService;
 import org.proxib.service.IClientService;
-import org.proxib.service.ServiceClient;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import com.sun.faces.renderkit.ClientSideStateHelper;
 
 public class MainGhania {
 
 	public static void main(String[] args) {
-		
+
 		ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
 
-	IAdviserService adviserService = context .getBean("serviceAdviser", IAdviserService.class);
-	Client c1 = new Client("bob", "marley", "paris", "@");
-	Client c2 = new Client("bobby", "marley", "paris", "@");
-	Adviser ad1 = new Adviser("bernard", "lermite");
-	Adviser ad2 = new Adviser("bernardo", "lermite");
-	ad1.addClientToAdviser(c1);
-	ad1.addClientToAdviser(c1);
-	ad1.addClientToAdviser(c2);
-	
-	try {
-		adviserService.persist(ad1);
-		adviserService.persist(ad2);
-	} catch (Exception e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
+		IAdviserService adviserService = context.getBean("serviceAdviser", IAdviserService.class);
+		IClientService clientService = context.getBean("serviceClient", IClientService.class);
+
+		Adviser ad1 = new Adviser("bernard", "lermite");
+		Adviser ad2 = new Adviser("bernardo", "lermite");
+
+		Client c1 = new Client("bob", "marley", "paris", "@");
+		Client c2 = new Client("bobby", "marley", "paris", "@");
+
+		CurrentAccount currentAccount = new CurrentAccount(575578.89, 0.6);
+
+		c1.addCurrentAccountToClient(currentAccount);
+		ad1.addClientToAdviser(c1);
+
+		ad1.addClientToAdviser(c2);
+
+		try {
+			adviserService.persist(ad1);
+			adviserService.persist(ad2);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
